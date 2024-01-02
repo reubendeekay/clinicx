6f27670512188d5af45b7a0bcd4f2309633e7b74
Halima-Ali <halimawario8@gmail.com>
2024-01-02 16:13:28 +0300

    Routing set up

--- clinicX/src/app/app.component.ts
+++ clinicX/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { CardComponent } from './Components/Card/card/card.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,CardComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- clinicX/src/app/app.routes.ts
+++ clinicX/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/Home/home/home.component';
+import { LoginComponent } from './Pages/Login/login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+];
