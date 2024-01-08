2b79943e95b3bfc129c4a577bc51226ddf8f3568
Halima-Ali <halimaali.ha2002@gmail.com>
2024-01-08 20:40:16 +0300

    login screens - not 100% height

--- clinicX/src/app/Components/topbar/topbar.component.ts
+++ clinicX/src/app/Components/topbar/topbar.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
   imports: [],
   templateUrl: './topbar.component.html',
-  styleUrl: './topbar.component.css'
+  styleUrl: './topbar.component.css',
 })
 export class TopbarComponent {
-
+  @Input() title: string | undefined;
 }
--- clinicX/src/app/Pages/dashboard-home/dashboard-home.component.ts
+++ clinicX/src/app/Pages/dashboard-home/dashboard-home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidebarComponent } from '../../Components/sidebar/sidebar.component';
+import { TopbarComponent } from '../../Components/topbar/topbar.component';
 
 @Component({
   selector: 'app-dashboard-home',
   standalone: true,
-  imports: [SidebarComponent],
+  imports: [SidebarComponent,TopbarComponent],
   templateUrl: './dashboard-home.component.html',
   styleUrl: './dashboard-home.component.css'
 })
