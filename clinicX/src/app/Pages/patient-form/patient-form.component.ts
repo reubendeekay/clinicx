1a7bf6c0408d75b153837438315ce9c2f2d7fa69
Halima-Ali <halimaali.ha2002@gmail.com>
2024-01-14 19:08:52 +0300

    addPatientForm components

--- clinicX/src/app/Pages/patient-form/patient-form.component.ts
+++ clinicX/src/app/Pages/patient-form/patient-form.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-
+import { TopbarComponent } from '../../Components/topbar/topbar.component';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { FileUploadModule } from 'primeng/fileupload';
 @Component({
   selector: 'app-patient-form',
   standalone: true,
-  imports: [],
+  imports: [TopbarComponent , ButtonModule, InputTextModule, FileUploadModule],
   templateUrl: './patient-form.component.html',
   styleUrl: '../dashboard-home/dashboard-home.component.css',
 })
